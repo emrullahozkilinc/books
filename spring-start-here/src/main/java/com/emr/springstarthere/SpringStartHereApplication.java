47861447c85f6efab602c86abf74945c458f578d
Emrullah <emr.ozk.71@gmail.com>
2023-01-02 13:16:10 +0300

    start-here added.

---
+++ spring-start-here/src/main/java/com/emr/springstarthere/SpringStartHereApplication.java
@@ -0,0 +1,13 @@
+package com.emr.springstarthere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStartHereApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringStartHereApplication.class, args);
+    }
+
+}
---
+++ spring-start-here/src/test/java/com/emr/springstarthere/SpringStartHereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emr.springstarthere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStartHereApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
